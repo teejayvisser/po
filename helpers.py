21431e30d26d7a91efc3ea8f3c9cebc55294aa82
theunis johannes visser <44087870+tjVisser@users.noreply.github.com>
2025-03-31 18:54:38 +0200

    -added additional logic to fetch individual pokemon

--- helpers.py
+++ helpers.py
@@ -1,37 +1,49 @@
 import requests
 import json
+from pprint import pprint
+
 
 #region Get all pokemon
 # initially went with the aproach of getting all pokemon and then getting all details but it was too slow 
-def getAllPokemon():
+def get_all_pokemon():
     '''Fetchs full list of all pokemon'''
     response = requests.get("https://pokeapi.co/api/v2/pokemon")
-    jsonCount = json.loads(response.content)
-    count =jsonCount['count'] 
+    json_count = json.loads(response.content)
+    count =json_count['count'] 
     
-    allPokemonListResponse = requests.get(f'https://pokeapi.co/api/v2/pokemon?limit={count}')
-    allPokemonList = json.loads(allPokemonListResponse.content)['results']
-    return allPokemonList  
+    all_pokemon_list_response = requests.get(f'https://pokeapi.co/api/v2/pokemon?limit={count}')
+    all_pokemon_list = json.loads(all_pokemon_list_response.content)['results']
+    return all_pokemon_list  
 
-def PopulatePokemonDetails(pokemonList):
+def populate_pokemon_details(pokemon_list):
     '''takes full list of pokemon and generates a new list with all pokemon details'''
-    fullList = []
-    for i in pokemonList:
+    full_list = []
+    for i in pokemon_list:
         response = requests.get(i['url'])
         result = json.loads(response.content)
-        fullList.append(result)
-    return fullList
+        full_list.append(result)
+    return full_list
+
+def fetch_individual_pokemon(name):
+    '''Fetches and returns an individual pokemon's full data object'''
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
+    if(response.status_code == 200): 
+        #I Considered mapping this to a Model object I am just not too sure how to go about this
+        pokemon = json.loads(response.content)
+        return pokemon
+    else:
+        raise Exception
 #endregion 
 
 
 def get_pokemon_types():
-    '''fetchs full list of pokemon types'''
+    '''fetches full list of pokemon types'''
     types_response = requests.get('https://pokeapi.co/api/v2/type')
     result = json.loads(types_response.content)
     return result 
 
 def get_pokemon_by_type(pokemon_object):
-    '''fetchs all pokemon by type'''
+    '''fetches all pokemon by type'''
     pokemon = requests.get(pokemon_object['url'])
     result = json.loads(pokemon.content)
     return result
@@ -55,8 +67,6 @@ def add_singular_pokemon_to_list(pokemon,poke_type,all_pokemon):
         all_pokemon[poke_type].append(poke_name)
     else:
         all_pokemon[poke_type] = ([poke_name]) 
-       
-         
     return all_pokemon
 
 def add_pokemon_to_full_list(pokemon_by_type,all_pokemon):
@@ -70,5 +80,13 @@ def add_pokemon_to_full_list(pokemon_by_type,all_pokemon):
             all_pokemon[poke_type].append(poke_name)
         else:
             all_pokemon[poke_type] = ([poke_name]) 
-         
     return all_pokemon
+
+def prompt_for_name(name):
+    '''prompts the user for a pokemon name with very basic error handling'''
+    try:
+        response = fetch_individual_pokemon(name)
+        pprint(response)
+    except:
+        user_input = input("it seems that pokemon could not be found perhaps try another name:")
+        prompt_for_name(user_input)
--- main.py
+++ main.py
@@ -5,17 +5,18 @@ import json
 type_list =helpers.get_pokemon_types()
 all_pokemon_primary={}
 all_pokemon_secondary={}
-allpoke = {}
+all_pokemone = {}
 
 #construct new dict using pokemon types
 for type in type_list['results']:
     print(f'fetch all pokemon for {type['name']}')
     pokemon_by_type =  helpers.get_pokemon_by_type(type)
-    allpoke = helpers.add_pokemon_to_full_list(pokemon_by_type,allpoke)
+    all_pokemone = helpers.add_pokemon_to_full_list(pokemon_by_type,all_pokemone)
     all_pokemon_primary,all_pokemon_secondary = helpers.add_pokemon_to_list(pokemon_by_type , all_pokemon_primary , all_pokemon_secondary)
 
 
 #writing pokemon to json files as it is easier to see and check
+print('Saving pokemon lists to json files for easier viewing')
 with open('Pokemon_primary.json', 'w') as fp:
     json.dump(all_pokemon_primary, fp)
 
@@ -23,10 +24,8 @@ with open('Pokemon_secondary.json', 'w') as fd:
     json.dump(all_pokemon_secondary, fd)
 
 with open('Pokemon_all.json', 'w') as fs:
-    json.dump(allpoke, fs)
-
+    json.dump(all_pokemone, fs)
     
 
-
-
-
+user_input = input("please type in a pokemon name you would like more details on:")
+helpers.prompt_for_name(user_input)
